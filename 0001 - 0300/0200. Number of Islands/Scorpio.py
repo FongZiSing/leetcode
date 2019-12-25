d30ffea3ac8a62b883f959c03913d2bd185773ad
SylarScorpio <49641550+SylarScorpio@users.noreply.github.com>
2019-12-25 16:00:49 +0800

    Delete Scorpio.py

--- 0001 - 0300/0200. Number of Islands/Scorpio.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i in range(len(nums)):
-            if target - nums[i] not in d:
-                d[nums[i]] = i
-            else:
-                return [d[target - nums[i]], i] 
