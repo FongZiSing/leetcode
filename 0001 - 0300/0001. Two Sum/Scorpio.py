7db2055250631ab3c51208dbabffe616f0631c4d
SylarScorpio <49641550+SylarScorpio@users.noreply.github.com>
2019-12-25 16:00:08 +0800

    Create Scorpio.py

---
+++ 0001 - 0300/0001. Two Sum/Scorpio.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(nums)):
+            if target - nums[i] not in d:
+                d[nums[i]] = i
+            else:
+                return [d[target - nums[i]], i] 
