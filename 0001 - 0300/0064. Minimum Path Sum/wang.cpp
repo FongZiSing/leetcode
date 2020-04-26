297442b40887554b9e0b7e3cc62e52caf91df730
WangYing <wwying1996@njust.edu.cn>
2020-04-26 10:41:22 +0800

    Create wang.cpp

---
+++ 0001 - 0300/0064. Minimum Path Sum/wang.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        // initialization
+        vector<vector<int>> dp(m);
+        for(int i = 0; i < m; i++)
+        {
+            dp[i].assign(n, 0);
+        }
+
+        // base case first row
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < n; i++)
+        {
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        // first column
+        for(int j = 1; j < m; j++)
+        {
+            dp[j][0] = dp[j-1][0] + grid[j][0];
+        }
+
+        // state transition
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
